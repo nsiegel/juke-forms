583743d2c636a1491dad98b4bf1b98fa39ed5b65
mliou8 <chunghanliou@gmail.com>
2015-12-07 17:43:32 -0500

    Reset forms, reading data

--- browser/js/controllers/new-playlist.js
+++ browser/js/controllers/new-playlist.js
@@ -1,5 +1,11 @@
 app.controller('PlaylistCtrl', function ($scope, PlaylistFactory) {
 	$scope.playListName = function (name) {
-    PlaylistFactory.create(name);
-  };
+	PlaylistFactory.create(name).then(function (data) {
+		$scope.reset();
+	});
+  }
+  $scope.reset = function () {
+  	$scope.playlist.name = '';
+  	$scope.formName.inputName.$setPristine();
+  	}
 });
--- browser/js/factories/new-playlist.js
+++ browser/js/factories/new-playlist.js
@@ -1,10 +1,8 @@
 app.factory('PlaylistFactory', function ($http) {
   var playlistFactory = {};
 	playlistFactory.create = function (data) {
-    console.log(data);
 		return $http.post('/api/playlists', data)
 		.then(function (response) {
-      console.log(response.data);
 			return response.data;
 		});
 	};
